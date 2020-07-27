731df2e404137ad11adfe838697bcf19f5aeee0c
prisae <dieter@werthmuller.org>
2020-07-27 17:44:49 +0200

    Fix it for good

--- benchmarks/time.py
+++ benchmarks/time.py
@@ -11,7 +11,39 @@ import numpy as np
 
 import emg3d
 
-VariableCatch = (LookupError, AttributeError, ValueError, TypeError, NameError)
+# Collect version info.
+INFO = emg3d.__version__.split('.')
+# Get dev-version.
+if len(INFO) > 3:
+    # Since b4de17e: setuptools: ?.?.?.dev?+ghash.
+    INFO = [*INFO[:3], INFO[3].split('+')[0][3:]]
+else:
+    # Before was ?.?.?dev? (but dev? mostly not updated).
+    INFO = [*INFO[:2], *INFO[2].split('dev')]
+if len(INFO) == 3:
+    # If release, add dev 999, so it does not fall below devs.
+    INFO = [*INFO, '999']
+INFO = tuple(map(int, INFO))
+
+# Version-dependent imports.
+# 59b4a89: 0.8.2.dev10  : Model frequency independent
+# d8e98c0: 0.9.1.dev4   : VolumeModel.
+# c9e595e: 0.9.3.dev1   : solver.solver => solver.solve
+# 8c31324: 0.10.2.dev1  : utils refactor to fields, meshes, models
+# 1b16955: 0.11.1.dev14 : res => property & mapping
+if INFO < (0, 9, 3, 1):
+    from emg3d.solver import solver as solve
+else:
+    from emg3d.solver import solve
+
+if INFO < (0, 10, 2, 1):
+    from emg3d.utils import Field, Model, TensorMesh, get_source_field
+    if INFO >= (0, 9, 1, 4):
+        from emg3d.utils import VolumeModel
+else:
+    from emg3d.fields import Field, get_source_field
+    from emg3d.models import Model, VolumeModel
+    from emg3d.meshes import TensorMesh
 
 # Load salt example data set
 DATA = np.load(join(dirname(__file__), 'data/salt_data.npz'),
@@ -38,69 +70,31 @@ def get_model(size, anisotropy='iso'):
     grid = TensorMesh([dat['hx'], dat['hy'], dat['hz']], dat['x0'])
 
     # Create model.
-    try:  # from 0.12.0 onwards, use property with mapping instead of res.
-        inp = {'grid': grid, 'property_x': dat['res'],
-               'mapping': 'Resistivity'}
-        if anisotropy == 'vti':
-            model = Model(property_z=3*dat['res'], **inp)
-        elif anisotropy == 'tri':
-            model = Model(property_y=2*dat['res'], property_z=3*dat['res'],
-                          **inp)
-        else:  # Default is 'iso'
-            model = Model(**inp)
-    except VariableCatch:
-        try:  # Model in emg3d < v0.9.0 was frequency dependent.
-            inp = {'grid': grid, 'res_x': dat['res'], 'freq': dat['freq']}
-            if anisotropy == 'vti':
-                model = Model(res_z=3*dat['res'], **inp)
-            elif anisotropy == 'tri':
-                model = Model(res_y=2*dat['res'], res_z=3*dat['res'], **inp)
-            else:  # Default is 'iso'
-                model = Model(**inp)
+    inp = {'grid': grid}
+    if INFO < (0, 11, 1, 14):  # resistivity, no mapping.
+        inp['res_x'] = dat['res']
+        if anisotropy in ['vti', 'tri']:
+            inp['res_z'] = 3*dat['res']
+            if anisotropy == 'tri':
+                inp['res_y'] = 2*dat['res']
 
-        except VariableCatch:  # Model in emg3d > v0.9.0 is freq. independent.
-            inp = {'grid': grid, 'res_x': dat['res']}
-            if anisotropy == 'vti':
-                model = Model(res_z=3*dat['res'], **inp)
-            elif anisotropy == 'tri':
-                model = Model(res_y=2*dat['res'], res_z=3*dat['res'], **inp)
-            else:  # Default is 'iso'
-                model = Model(**inp)
+        if INFO < (0, 8, 2, 10):  # Frequency-dependent Model class.
+            inp['freq'] = dat['freq']
+
+    else:  # property+mapping instead of res.
+        inp['property_x'] = dat['res']
+        inp['mapping'] = 'Resistivity'
+        if anisotropy in ['vti', 'tri']:
+            inp['property_z'] = 3*dat['res']
+            if anisotropy == 'tri':
+                inp['property_y'] = 2*dat['res']
+
+    model = Model(**inp)
 
     # Create source field.
     sfield = get_source_field(grid, dat['src'], dat['freq'], 0)
 
     return grid, model, sfield
-
-
-# Change from emg3d.solver.solver to emg3d.solver.solve.
-try:
-    from emg3d.solver import solve
-except ImportError:
-    from emg3d.solver import solver as solve
-
-# Change from emg3d.utils to other modules.
-try:
-    from emg3d.utils import Field, Model, TensorMesh, get_source_field
-
-    # Find out if we are in the before eef25f71 or not.
-    grid, tmodel, sfield = get_model('small')
-    try:
-        try:  # Needs VolumeModel from d8e98c0 onwards.
-            model = emg3d.utils.VolumeModel(grid, tmodel, sfield)
-        except AttributeError:
-            model = tmodel
-        a, b = emg3d.solver.residual(grid, model, sfield, sfield*0)
-        BEFORE = False
-    except ValueError:
-        BEFORE = True
-    del grid, sfield, tmodel, model
-
-except ImportError:
-    BEFORE = True
-    from emg3d.fields import Field, get_source_field
-    from emg3d.models import Model, VolumeModel
-    from emg3d.meshes import TensorMesh
 
 
 class SolverTimeSSL:
@@ -217,10 +211,11 @@ class SmoothingTime:
             grid, model, sfield = get_model(size)
             data[size]['grid'] = grid
             data[size]['sfield'] = sfield
-            try:  # Needs VolumeModel from d8e98c0 onwards.
-                data[size]['model'] = VolumeModel(grid, model, sfield)
-            except AttributeError:
+            # Needs VolumeModel from 0.9.1dev4 / d8e98c0 onwards.
+            if INFO < (0, 9, 1, 4):
                 data[size]['model'] = model
+            else:
+                data[size]['model'] = VolumeModel(grid, model, sfield)
         return data
 
     def time_smoothing(self, data, lr_dir, size):
@@ -229,12 +224,8 @@ class SmoothingTime:
         sfield = Field(grid, data[size]['sfield'])
         efield = Field(grid)
         inp = (grid, model, sfield, efield, 2, lr_dir)
-        if BEFORE:
-            emg3d.solver.smoothing(*inp)
-            res = emg3d.solver.residual(grid, model, sfield, efield)
-            norm = np.linalg.norm(res)
-        else:  # After, residual is included in smoothing and norm in residual.
-            res, norm = emg3d.solver.smoothing(*inp)
+        emg3d.solver.smoothing(*inp)
+        _ = emg3d.solver.residual(grid, model, sfield, efield)
 
 
 class ResidualTime:
@@ -249,20 +240,15 @@ class ResidualTime:
             grid, model, sfield = get_model(size)
             data[size]['grid'] = grid
             data[size]['sfield'] = sfield
-            try:  # Needs VolumeModel from d8e98c0 onwards.
-                data[size]['model'] = VolumeModel(grid, model, sfield)
-            except AttributeError:
+            # Needs VolumeModel from 0.9.1dev4 / d8e98c0 onwards.
+            if INFO < (0, 9, 1, 4):
                 data[size]['model'] = model
-
+            else:
+                data[size]['model'] = VolumeModel(grid, model, sfield)
         return data
 
     def time_residual(self, data, size):
         grid = data[size]['grid']
         model = data[size]['model']
         sfield = Field(grid, data[size]['sfield'])
-        if BEFORE:
-            res = emg3d.solver.residual(grid, model, sfield, sfield.field*0)
-            norm = np.linalg.norm(res)
-        else:  # After, norm is included in residual.
-            res, norm = emg3d.solver.residual(
-                    grid, model, sfield, sfield.field*0)
+        _ = emg3d.solver.residual(grid, model, sfield, sfield.field*0)
